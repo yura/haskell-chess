973efcb78262b1fb0af356ca063b6b021dda7bca
Yury Kotlyarov <yury.kotlyarov@gmail.com>
2021-11-16 16:29:24 +0300

    feat: Ready to render on the terminal screen

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,17 @@
 module Main where
 
-import Lib
+import Control.Concurrent (threadDelay)
+import Data.Char
+
+clearScreen :: IO ()
+clearScreen = do
+  putStrLn $ chr 27 : "[2J"
+  putStrLn $ chr 27 : "[;H"
 
 main :: IO ()
-main = someFunc
+main = do
+  clearScreen
+  putStrLn "Hello chess"
+  threadDelay 1000000
+  clearScreen
+  putStrLn "Hey"
