864342c6192e52cb49032e7d3b1cc1a4d6c1c272
Yury Kotlyarov <yury.kotlyarov@gmail.com>
2021-11-16 10:48:37 +0300

    feat: Refactoring of square names and few functons for diagonal moves

--- src/Board.hs
+++ src/Board.hs
@@ -1,26 +1,47 @@
 module Board where
 
 import qualified Data.Map as M
-import           Data.Text
 
 data PieceType = King | Queen | Rook | Bishop | Knight | Pawn deriving (Eq, Show)
 data PieceColor = White | Black deriving (Eq, Show)
 data Piece = Piece PieceType PieceColor deriving (Eq, Show)
 
-data Square = Square Text (Maybe Piece) deriving (Eq, Show)
-data Board = Board (M.Map Text Square)
+type SquareName = (Char, Int)
+data Square = Square SquareName (Maybe Piece) deriving (Eq, Show)
+data Board = Board (M.Map SquareName Square)
 
-squareName :: Char -> Int -> Text
-squareName row col = singleton row <> pack (show col)
+rowNames = ['a'..'h']
+colNames = [1..8]
 
-squareNames = [squareName row col | row <- ['a'..'h'], col <- [1..8]]
+squareNames :: [SquareName]
+squareNames = [(row, col) | row <- rowNames, col <- colNames]
 
 emptyBoard :: Board
 emptyBoard = Board $ M.fromList rows
   where
     rows = Prelude.map (\squareName -> (squareName, Square squareName Nothing)) squareNames
 
-placePiece :: Text -> Piece -> Board -> Board
+placePiece :: SquareName -> Piece -> Board -> Board
 placePiece squareName piece (Board squares) = Board $ M.insert squareName (Square squareName $ Just piece) squares
 
+bottomLeft :: SquareName -> [SquareName]
+bottomLeft (row, col)  = zip [pred row, pred (pred row)..head rowNames] [pred col, pred (pred col)..head colNames]
+
+bottomRight :: SquareName -> [SquareName]
+bottomRight (row, col) = zip [succ row..last rowNames] [pred col, pred (pred col)..head colNames]
+
+topRight :: SquareName -> [SquareName]
+topRight (row, col)    = zip [succ row..last rowNames] [succ col.. last colNames]
+
+topLeft :: SquareName -> [SquareName]
+topLeft (row, col)     = zip [pred row, pred (pred row)..head rowNames] [succ col..last colNames]
+
+--bishopPossibleMoves :: SquareName -> [SquareName]
+--bishopPossibleMoves squareName = bottomLeft squareName ++ bottomRight squareName ++ topRight squareName ++ topLeft squareName
+
+----
+{-
+squareName :: (Char, Int) -> Text
+squareName (row, col) = singleton row <> pack (show col)
+-}
 
--- test/BoardSpec.hs
+++ test/BoardSpec.hs
@@ -5,6 +5,7 @@ module BoardSpec (spec) where
 import Test.Hspec ( describe, it, shouldBe, Spec, pending )
 
 import qualified Data.Map as M
+import Data.List ( nub )
 import Board
 
 spec :: Spec
@@ -23,10 +24,78 @@ spec = do
   describe "placePiece" $ do
     it "уставнавливает фигуру на заданную клетку" $ do
       let whitePawn = Piece Pawn White
-      let (Board squares) = placePiece "d4" whitePawn emptyBoard
-      M.lookup "d4" squares `shouldBe` Just (Square "d4" $ Just whitePawn)
+      let (Board squares) = placePiece ('d', 4) whitePawn emptyBoard
+      M.lookup ('d', 4) squares `shouldBe` Just (Square ('d', 4) $ Just whitePawn)
+
+
+  describe "bottomLeft" $ do
+    it "возращает все имена из центра в начало по главной диагонали" $
+      bottomLeft ('d', 4) `shouldBe` [('c', 3), ('b', 2), ('a', 1)]
+
+    it "возращает пустой список из 'a2'" $
+      bottomLeft ('a', 2) `shouldBe` []
+
+    it "возращает пустой список из 'b1'" $
+      bottomLeft ('b', 1) `shouldBe` []
+
+    it "возращает 'a7' из 'b8'" $
+      bottomLeft ('b', 8) `shouldBe` [('a', 7)]
+
+    it "возращает главную диагональ из 'h8'" $
+      bottomLeft ('h', 8) `shouldBe` [('g', 7), ('f', 6), ('e', 5), ('d', 4), ('c', 3), ('b', 2), ('a', 1)]
+
+    it "возращает пустой список из 'a1..a8' и 'a1..h1'" $ do
+      let names = [(r, 1) | r <- ['a'..'h']] ++ [('a', c) | c <- [1..8]]
+      let result = map (\n -> bottomLeft n) names
+      nub result `shouldBe` [[]]
+      
+  describe "bottomRight" $ do
+    it "возращает все имена из центра в начало по главной диагонали" $
+      bottomRight ('d', 4) `shouldBe` [('e', 3), ('f', 2), ('g', 1)]
+
+    it "возращает 'b1' из 'a2'" $
+      bottomRight ('a', 2) `shouldBe` [('b', 1)]
+
+    it "возращает пустой список из 'b1'" $
+      bottomRight ('b', 1) `shouldBe` []
+
+    it "возращает всю диагональ из 'b8'" $
+      bottomRight ('b', 8) `shouldBe` [('c', 7), ('d', 6), ('e', 5), ('f', 4), ('g', 3), ('h', 2)]
+
+    it "возращает пустой список из 'h8'" $
+      bottomRight ('h', 8) `shouldBe` []
+
+    it "возращает пустой список из 'a1..h1' и 'h1..h8'" $ do
+      let names = [(r, 1) | r <- ['a'..'h']] ++ [('h', c) | c <- [1..8]]
+      let result = map (\n -> bottomRight n) names
+      nub result `shouldBe` [[]]
+
+  describe "topRight" $ do
+    it "возращает все имена из центра в конец по главной диагонали" $
+      topRight ('d', 4) `shouldBe` [('e', 5), ('f', 6), ('g', 7), ('h', 8)]
+
+    it "возращает всю диагональ из 'a2'" $
+      topRight ('a', 2) `shouldBe` [('b', 3), ('c', 4), ('d', 5), ('e', 6), ('f', 7), ('g', 8)]
+
+    it "возращает пустой список из 'a8..h8' и 'h1..h8'" $ do
+      let names = [(r, 8) | r <- ['a'..'h']] ++ [('h', c) | c <- [1..8]]
+      let result = map (\n -> topRight n) names
+      nub result `shouldBe` [[]]
+
+  describe "topLeft" $ do
+    it "возращает все имена из центра в конец по главной диагонали" $
+      topLeft ('d', 4) `shouldBe` [('c', 5), ('b', 6), ('a', 7)]
+
+    it "возращает всю диагональ из 'h4'" $
+      topLeft ('h', 4) `shouldBe` [('g', 5), ('f', 6), ('e', 7), ('d', 8)]
+
+    it "возращает пустой список из 'a8..h8' и 'h1..h8'" $ do
+      let names = [(r, 8) | r <- ['a'..'h']] ++ [('a', c) | c <- [1..8]]
+      let result = map (\n -> topLeft n) names
+      nub result `shouldBe` [[]]
 
   describe "bishopPossibleMoves" $ do
-    it "" $
+    it "все возможные ходы слона, если слон находится в центре" $ do
       pending
+      --bishopPossibleMoves emptyBoard "d4" `shouldBe` [ "a1", "b2", "c3", "e5", "f6", "g7", "h8", "g1", "f2", "e3", "c5" "b6" "a7" ]  
 
