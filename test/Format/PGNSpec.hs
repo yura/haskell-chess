7d13ac97659a600078bb1b8176916943f4471765
Yury Kotlyarov <yury.kotlyarov@gmail.com>
2022-01-20 20:11:50 +0500

    fix: Draw

--- test/Format/PGNSpec.hs
+++ test/Format/PGNSpec.hs
@@ -30,7 +30,7 @@ spec = do
       parseResult' "0-1" `shouldParse` BlackWon
 
     it "возвращает ничью чёрных, если результат записан в виде '1/2-1/2'" $
-      parseResult' "0-1" `shouldParse` Draw
+      parseResult' "1/2-1/2" `shouldParse` Draw
 
   describe "parseMove" $ do
     let parseMove' = parse parseMove "PGN"
