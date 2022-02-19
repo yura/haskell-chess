d2cb35ee3ecf4cb80c00489b0f2cffc545588a5d
Yury Kotlyarov <yury.kotlyarov@gmail.com>
2022-02-19 17:03:00 +0300

    feat: Greedy bot - greedy captures pieces, but unable to check and checkmate

---
+++ src/Bot/Greedy.hs
@@ -0,0 +1,51 @@
+module Bot.Greedy where
+
+import Board
+import Laws
+import Bot.Random
+
+makeMove :: Color -> Board -> [Move] -> IO (Board, [Move])
+makeMove color board history = do
+  let moves = possibleMoves color board
+  let maxMove = maximum moves
+  random <- randomMove moves
+
+  return $ case maxMove of
+      Move {} -> (move board random, random:history)
+      _       -> (move board maxMove, maxMove:history)
+
+instance Ord Move where
+  compare (CapturePromotion _ _ (Piece Queen _)) (CapturePromotion _ _ (Piece Queen _)) = EQ
+  compare (CapturePromotion _ _ (Piece Queen _)) _                                      = GT
+  compare _                                      (CapturePromotion _ _ (Piece Queen _)) = LT
+
+  compare CapturePromotion {}                    CapturePromotion {}                    = EQ
+  compare CapturePromotion {}                    _                                      = GT
+  compare _                                      CapturePromotion {}                    = LT
+
+  compare (Promotion _ _ (Piece Queen _))        (Promotion _ _ (Piece Queen _))        = EQ
+  compare (Promotion _ _ (Piece Queen _)) _                                             = GT
+  compare _                                      (Promotion _ _ (Piece Queen _))        = LT
+
+  compare Promotion {}                           Promotion {}                           = EQ
+  compare Promotion {}                             _                                    = GT
+  compare _                                      Promotion {}                           = LT
+
+  compare EnPassantCapture {}                    EnPassantCapture {}                    = EQ
+  compare EnPassantCapture {}                    _                                      = GT
+  compare _                                      EnPassantCapture {}                    = LT
+
+  compare Capture {}                             Capture {}                             = EQ
+  compare Capture {}                             _                                      = GT
+  compare _                                      Capture {}                             = LT
+
+  compare KingsideCastling {}                    KingsideCastling {}                    = EQ
+  compare KingsideCastling {}                    _                                      = GT
+  compare _                                      KingsideCastling {}                    = LT
+
+  compare QueensideCastling {}                   QueensideCastling {}                   = EQ
+  compare QueensideCastling {}                   _                                      = GT
+  compare _                                      QueensideCastling {}                   = LT
+
+  compare _                                      _                                      = EQ
+
