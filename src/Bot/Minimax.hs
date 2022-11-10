dc9f5fd40d80eeb88eb35aef58d2d9e3dee7c3a2
Yury Kotlyarov <yury.kotlyarov@gmail.com>
2022-11-10 14:30:03 +0500

    refactor: Упрощение Minimax

--- src/Bot/Minimax.hs
+++ src/Bot/Minimax.hs
@@ -6,31 +6,18 @@ import Data.List (sortOn)
 import Data.Maybe (isJust)
 import Board
 import Laws
--- import Bot.Random
 import Evaluation (Value, discount, evaluatePosition)
 
 makeMove :: Board -> IO Board
 makeMove board@Board{..} = do
-  let (v1, m1) = maxValue board 1
-  let (v3, m3) = maxValue board 3
-
-  --return $ if v1 == abs mateValue then move board m1 else move board m3
-  return $ move board m3
+  let (_, m) = maxValue board 3
+  return $ move board m
 
 maxValue :: Board -> Int -> (Value, Move)
-maxValue board depth | isJust (isOver board) || depth == 0 = (evaluatePosition board, head $ history board)
-                     | otherwise = (v - discount, m)
+maxValue board depth | isJust (isOver board) || depth == 0 = (evaluatePosition board - depth, head $ history board)
+                     | otherwise = (v, m)
   where
     (v, m) = last results
     results = sortOn fst
-      $ map (\m -> ((-1) * fst (minValue (move board m) (pred depth)) - discount, m))
-      $ possibleMoves board
-
-minValue :: Board -> Int -> (Value, Move)
-minValue board depth | isJust (isOver board) || depth == 0 = (evaluatePosition board, head $ history board)
-                     | otherwise = (v - discount, m)
-  where
-    (v, m) = last results
-    results = sortOn fst
-      $ map (\m -> ((-1) * fst (maxValue (move board m) (pred depth)), m))
+      $ map (\m -> ((-1) * fst (maxValue (move board m) (depth - 1)), m))
       $ possibleMoves board
--- test/Bot/MinimaxSpec.hs
+++ test/Bot/MinimaxSpec.hs
@@ -15,19 +15,19 @@ spec = do
   describe "maxValue" $ do
     context "[начальная позиция]" $ do
       it "возращает 0 как максимальное возможное значение для хода для глубины 1" $ do
-        fst (maxValue initialBoard 1) `shouldBe` (-2)
+        fst (maxValue initialBoard 1) `shouldBe` 0
 
       it "возращает 0 как максимальное возможное значение для хода для глубины 2" $ do
-        fst (maxValue initialBoard 1) `shouldBe` (-2)
+        fst (maxValue initialBoard 1) `shouldBe` 0
 
     context "[белые]" $ do
       context "[мат в один ход]" $ do
         it "возращает максимально возможное значение как максимальное возможное значение для хода для глубины 1" $ do
-          fst (maxValue blackMatedByQueenIn1 1) `shouldBe` (-mateValue - 2 * discount)
+          fst (maxValue blackMatedByQueenIn1 1) `shouldBe` (-mateValue)
 
       context "[мат в два хода]" $ do
         it "находит линейный мат в два хода" $ do
-          maxValue whiteLadderMatesIn2 3 `shouldBe` (-mateValue - 3 * discount, Move (Piece Rook White) ('e',1) ('g',1))
+          maxValue whiteLadderMatesIn2 3 `shouldBe` (-mateValue, Move (Piece Rook White) ('e',1) ('g',1))
 
       context "[мат в 3 хода]" $ do
         it "находит линейный мат в три хода" $ do
@@ -44,21 +44,21 @@ spec = do
       context "[мат один ход (github issue #1)]" $ do
         it "ход 49" $ do
           let board = fromFEN "8/7K/1Q6/8/k7/2p5/PPPP1PPP/1RB3NR w - - 0 49"
-          maxValue board 3 `shouldBe` (-mateValue - 2 * discount, Move (Piece Pawn White) ('b', 2) ('b', 3))
+          maxValue board 3 `shouldBe` (-mateValue + 2, Move (Piece Pawn White) ('b', 2) ('b', 3))
 
     context "[чёрные]" $ do
       context "[мат в один ход]" $ do
         it "возращает максимально возможное значение оценки и ход конём" $ do
-          maxValue whiteMatedByKninghtIn1 1 `shouldBe` (-mateValue - 2 * discount, Move knightBlack ('a', 3) ('c', 2))
+          maxValue whiteMatedByKninghtIn1 1 `shouldBe` (-mateValue, Move knightBlack ('a', 3) ('c', 2))
 
       context "[мат в два хода]" $ do
         it "находит линейный мат в два хода" $ do
-          maxValue blackLadderMatesIn2 3 `shouldBe` (-mateValue - 3 * discount, Move (Piece Rook Black) ('e',1) ('g',1))
+          maxValue blackLadderMatesIn2 3 `shouldBe` (-mateValue, Move (Piece Rook Black) ('e',1) ('g',1))
 
       context "[мат в три хода]" $ do
         it "находит линейный мат в два хода" $ do
           pendingWith "Slow test"
-          maxValue blackLadderMatesIn3 5 `shouldBe` (-mateValue - 3 * discount, Move (Piece Rook Black) ('d',2) ('f',2))
+          maxValue blackLadderMatesIn3 5 `shouldBe` (-mateValue, Move (Piece Rook Black) ('d',2) ('f',2))
 
       context "защита от мата в 1 ход" $ do
         it "чёрные должны найти возможный мат в 2 хода, белые должны найти защиту от мата в 1 ход" $ do
